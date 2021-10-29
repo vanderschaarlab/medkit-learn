afd703ec9cbd1ed000392ec6b1083e8a4bb06400
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-10-29 12:34:02 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="medkit_learn",
-    version="0.1.0",
+    version="0.1.1",
     author="Alex J. Chan",
     author_email="alexjameschan@gmail.com",
     description="Medical sequential decision making simulation tools.",
